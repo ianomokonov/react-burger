02b991a3f0464e1079dccaab0b66dadcc46638fe
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2023-04-05 22:16:07 +0300

    профиль

--- src/pages/profile/edit-form/edit-form.tsx
+++ src/pages/profile/edit-form/edit-form.tsx
@@ -1,5 +1,56 @@
-import { FC } from "react";
+import { Input } from "@ya.praktikum/react-developer-burger-ui-components";
+import { FC, useState } from "react";
 
 export const EditForm: FC = () => {
-    return <></>
-}
+  const [formValue, setFormValue] = useState({
+    name: "Иван",
+    email: "nomokonov.vana@yandex.ru",
+    password: "1234",
+  });
+  return (
+    <>
+      <Input
+        type={"text"}
+        placeholder={"Имя"}
+        onChange={(e) =>
+          setFormValue((prev) => ({ ...prev, name: e.target.value }))
+        }
+        value={formValue.name}
+        disabled
+        icon={"EditIcon"}
+        name={"name"}
+        error={false}
+        size={"default"}
+        extraClass="mb-6"
+      />
+      <Input
+        type={"email"}
+        placeholder={"Login"}
+        onChange={(e) =>
+          setFormValue((prev) => ({ ...prev, email: e.target.value }))
+        }
+        value={formValue.email}
+        disabled
+        name={"email"}
+        icon={"EditIcon"}
+        error={false}
+        size={"default"}
+        extraClass="mb-6"
+      />
+      <Input
+        type="password"
+        placeholder={"Пароль"}
+        onChange={(e) =>
+          setFormValue((prev) => ({ ...prev, password: e.target.value }))
+        }
+        value={formValue.password}
+        disabled
+        icon={"EditIcon"}
+        name={"password"}
+        error={false}
+        size={"default"}
+        extraClass="mb-6"
+      />
+    </>
+  );
+};
--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,11 +1,48 @@
 import { FC } from "react";
 import styles from "./profile.module.css";
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export const Profile: FC = () => {
   return (
     <div className={styles.main}>
-      <div className={styles.menu}></div>
+      <div className={styles.menu}>
+        <NavLink
+          className={({ isActive }) =>
+            `text text_type_main-medium ${styles.menu__item} ${
+              isActive ? "" : styles.text_color_inactive
+            }`
+          }
+          to="/profile"
+        >
+          Профиль
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `text text_type_main-medium ${styles.menu__item} ${
+              isActive ? "" : styles.text_color_inactive
+            }`
+          }
+          to="/orders-history"
+        >
+          История заказов
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `text text_type_main-medium ${styles.menu__item} ${
+              isActive ? "" : styles.text_color_inactive
+            }`
+          }
+          to="/logout"
+        >
+          Выход
+        </NavLink>
+        <span
+          className="text text_type_main-default"
+          style={{ opacity: "0.4" }}
+        >
+          В этом разделе вы можете изменить свои персональные данные
+        </span>
+      </div>
       <div className={styles.content}>
         <Outlet />
       </div>
